553ef119ac6bfc8e88509020b80aadc54901e2bf
surajshelke03 <surajshelkedev@gmail.com>
2024-08-21 23:35:41 +0530

    Responsive Nav-to-Sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import Homepage from './components/Homepage/Homepage';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 
 function App() {
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Navbar.css';
+import Sidebar from '../Sidebar/Sidebar';
+
 
 const Navbar = () => {
   return (
@@ -8,16 +10,17 @@ const Navbar = () => {
         <img src="https://www.rockwellautomation.com/content/dam/rockwell-automation/sites/images/logos/2019_Logo_rgb_RA_Bug-LeftText_color.svg" alt="Rockwell Automation" />
       </div>
       <ul className="navbar-links">
-        <li><a href="/">Products</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About Us</a></li>
         <li><a href="/">Services</a></li>
-        <li><a href="/">Solutions & Industries</a></li>
-        <li><a href="/">Support</a></li>
-        <li><a href="/">Sales & Partners</a></li>
+        <li><a href="/">Blog</a></li>
+        <li><a href="/">Help Center</a></li>
       </ul>
       <div className="navbar-icons">
-        <a href="/login"><i className="fa fa-user"></i></a>
         <a href="/search"><i className="fa fa-search"></i></a>
+        <a href="/login"><i className="fa fa-user"></i></a>
       </div>
+      <Sidebar />
     </nav>
   );
 }
